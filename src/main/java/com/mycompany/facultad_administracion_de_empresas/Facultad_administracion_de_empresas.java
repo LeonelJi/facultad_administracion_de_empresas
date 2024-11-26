faffa75a702943951ff76d6dc2be82018e27a0b4
LeonelJi <lj924618@gmail.com>
2024-11-26 10:38:32 -0500

    hola

---
+++ src/main/java/com/mycompany/facultad_administracion_de_empresas/Facultad_administracion_de_empresas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.facultad_administracion_de_empresas;
+
+/**
+ *
+ * @author User
+ */
+public class Facultad_administracion_de_empresas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
